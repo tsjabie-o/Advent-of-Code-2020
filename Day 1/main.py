df7d4cd927c78b270eb301289289cb6ea0b0762c
Xavi Oorthuis <x.e.oorthuis@gmail.com>
2021-05-20 15:52:06 +0200

    Part two Day 1 solved

--- Day 1/main.py
+++ Day 1/main.py
@@ -4,7 +4,15 @@ with open(".\Day 1\data.txt", "r") as input:
     for x in input:
         data.append(int(x))
 
+# Pair of two
 for i in range(len(data)):
     for j in range(i+1, len(data)):
         if data[i] + data[j] == 2020:
             print(data[i] * data[j])
+
+# Pair of three
+for i in range(len(data)):
+    for j in range(i+1, len(data)):
+        for k in range(j+1, len(data)):
+            if data[i] + data[j] + data[k] == 2020:
+                print (data[i] * data[j] * data[k])
