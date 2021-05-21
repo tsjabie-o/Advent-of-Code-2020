965fe59fd81d8bc133355e9f88d1544f7bb48c16
Xavi Oorthuis <x.e.oorthuis@gmail.com>
2021-05-21 11:09:49 +0200

    Day 3 Part 2 solution

--- Day 3/main.py
+++ Day 3/main.py
@@ -3,16 +3,25 @@ with open("./Day 3/data.txt", "r") as input:
     for line in input:
         data.append(line)
 
-x_pos = 0
-y_pos = 0
-trees = 0
-while y_pos < len(data):
-    if x_pos >= len(data[y_pos]) - 1:
-        x_pos = x_pos % (len(data[y_pos]) - 1)
-    
-    if data[y_pos][x_pos] == '#':
-        trees += 1
-    x_pos += 3
-    y_pos += 1
 
-print(trees)
+def CountTrees(y_change, x_change):
+    x_pos = 0
+    y_pos = 0
+    trees = 0
+    while y_pos < len(data):
+        if x_pos >= len(data[y_pos]) - 1:
+            x_pos = x_pos % (len(data[y_pos]) - 1)
+        
+        if data[y_pos][x_pos] == '#':
+            trees += 1
+        x_pos += x_change
+        y_pos += y_change
+    return trees
+
+print(
+    CountTrees(1, 3) * 
+    CountTrees(1,1) * 
+    CountTrees(1, 5) * 
+    CountTrees(1, 7) * 
+    CountTrees(2, 1)
+    )
