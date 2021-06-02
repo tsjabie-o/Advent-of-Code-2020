f4b699f6925adfc212552345411db9c055a8fc76
Xavi Oorthuis <x.e.oorthuis@gmail.com>
2021-06-02 19:34:48 +0200

    day 4 part 1 solution

--- Day 4/main.py
+++ Day 4/main.py
@@ -1,25 +1,33 @@
-import re
+passports = list()
+
+with open("./Day 4/data.txt", "r") as inpt:
+    data = inpt.read()
+
+lines = data.split("\n\n")
+lines = [line.replace("\n", " ") for line in lines]
+
+passports = list()
+
+for line in lines:
+    passport = dict()
+    fields = line.split()
+    for field in fields:
+        key, value = field.split(":")
+        passport[key] = value
+    passports.append(passport)
 
 validPassports = 0
 
-with open("./Day 4/data.txt", "r") as input:
-    amount = 0
-    cidMiss = True
-
-    for line in input:
-        if not line.strip():
-            if amount == 8 or (amount == 7 and cidMiss):
-                print("found valid, amount = {}, and cidmiss is: {}".format(amount, cidMiss))
-                validPassports += 1
-            cidMiss = True
-            amount = 0
-            continue
-        if re.search(r'cid:', line):
-            cidMiss = False
-        amount += len(line.split())
-        print("line: " + line, end="")
-        print("Found {} entries".format(len(line.split())))
+for passport in passports:
+    cid = False
+    if "cid" in passport:
+        cid = True
+    amount = len(passport.keys())
+    if amount == 8 or (cid == False and amount == 7):
+        validPassports += 1
 
 print(validPassports)
+
+
 
         
