0ca0afd621aa7fd85fa109953233a6c04d378503
Xavi Oorthuis <x.e.oorthuis@gmail.com>
2021-06-03 23:32:55 +0200

    working on day 4 p 2

--- Day 4/main.py
+++ Day 4/main.py
@@ -16,7 +16,7 @@ for line in lines:
         passport[key] = value
     passports.append(passport)
 
-validPassports = 0
+validPassports = list()
 
 for passport in passports:
     cid = False
@@ -24,10 +24,26 @@ for passport in passports:
         cid = True
     amount = len(passport.keys())
     if amount == 8 or (cid == False and amount == 7):
-        validPassports += 1
+        validPassports.append(passport)
 
-print(validPassports)
+print(len(validPassports))
+
+validPassports2 = list()
+
+for passport in validPassports:
+    if (
+        int(passport["byr"]) >= 1920 and int(passport["byr"]) <= 2002
+        and int(passport["iyr"]) >= 2010 and int(passport["iyr"]) <= 2020
+        and int(passport["eyr"]) >= 2020 and int(passport["eyr"]) <= 2030
 
 
 
-        
+        and passport["hgt"][-2:] == "cm" or passport["hgt"][-2:] == "in"
+
+
+
+        and int(passport["iyr"]) >= 2010 and int(passport["iyr"]) <= 2020
+        and int(passport["iyr"]) >= 2010 and int(passport["iyr"]) <= 2020
+        and int(passport["iyr"]) >= 2010 and int(passport["iyr"]) <= 2020
+        and int(passport["iyr"]) >= 2010 and int(passport["iyr"]) <= 2020
+    )
